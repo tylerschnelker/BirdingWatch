aa1611845e7a6a5fb09d3304386020e77865cd6b
Tyler Schenlker <tylerschnelker@yahoo.com>
2026-05-14 19:54:43 -0700

    main

--- server/main.py
+++ server/main.py
@@ -66,8 +66,7 @@ async def upload_audio(file: UploadFile = File(...), x_api_key: str = Header(Non
         
         # Process each detection
         if len(detections) == 0:
-            # os.remove(filepath)
-            pass
+            os.remove(filepath)
             print(f"No birds detected, deleted {filename}")
             return JSONResponse({
                 "status": "ok",
